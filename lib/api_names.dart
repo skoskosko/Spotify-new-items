87b2387599978c8790277a532c74ed875a561070
Tero Mielikäinen <terppa.mielikainen@hotmail.com>
2021-11-30 19:02:51 +0200

    Added windows and web support, package updates

--- lib/api_names.dart
+++ lib/api_names.dart
@@ -2,7 +2,7 @@ class ApiUrls {
   static String ownFollowedPlaylists = "https://api.spotify.com/v1/me/playlists";
   static String followedArtists = "https://api.spotify.com/v1/me/following?type=artist&limit=50";
   static String newReleases = "https://api.spotify.com/v1/browse/new-releases?limit=50";
-  static String play = "https://api.spotify.com/v1/me/player/play";
+  static Uri play = Uri.parse("https://api.spotify.com/v1/me/player/play");
   static String pause = "https://api.spotify.com/v1/me/player/pause";
   static String addToQueue(String trackUri) { return "https://api.spotify.com/v1/me/player/queue?uri=$trackUri"; }
   static String artistAlbums(String artistId, String offset) { return "https://api.spotify.com/v1/artists/$artistId/albums?offset=$offset&limit=20&include_groups=single"; }
